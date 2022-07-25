9648dd967bba5c0b86c883ecde74cdb2aeea98f0
Oleg <swinkin55@yandex.ru>
2022-07-25 19:46:53 +0300

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,15 +1,9 @@
 import pickle
 import os
 
-def dump_embeddings_to_hdd(model_embeddings):
-    f = open('model_embeddings', 'wb')
-    pickle.dump(model_embeddings, f)
-    path = os.path.realpath(f.name)
-    f.close()
-    return path
 
-def eval_data_size(path):
-    f = open(path, 'rb')
-    size = os.fstat(f.fileno()).st_size
-    f.close()
-    return size
+def get_embeddings_hdd_size(model_embeddings, path):
+    with open(path, 'wb') as f:
+        pickle.dump(model_embeddings, f)
+        hdd_size = os.fstat(f.fileno()).st_size
+    return hdd_size
